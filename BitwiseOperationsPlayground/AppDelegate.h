9d46636f71b511155e7603b0bdf15fa0a8663b70
James Alan Bush <jamesalanbush1972@gmail.com>
2022-01-02 16:33:22 -0500

    Initial Commit

---
+++ BitwiseOperationsPlayground/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BitwiseOperationsPlayground
+//
+//  Created by Xcode Developer on 1/2/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BitwiseOperationsPlayground/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BitwiseOperationsPlayground
+//
+//  Created by Xcode Developer on 1/2/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
